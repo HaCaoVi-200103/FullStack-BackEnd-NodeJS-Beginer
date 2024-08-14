b5656e8dfb72f54aac41421d03b8fb9c12a0670a
Cao Vy <vihcce170371@fpt.edu.vn>
2024-08-15 01:50:59 +0700

    setup static file to show for client

--- src/server.js
+++ src/server.js
@@ -18,8 +18,14 @@ const app = express();
 const port = process.env.PORT;
 const hostname = process.env.HOST_NAME;
 const path = require('path')
+
+//config template engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+//config static file
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'assets')))
 
 app.get('/', (req, res) => {
     res.send('Hello Express vs ICao Vy!!!')
