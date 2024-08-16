f6a4cbfa7a3586d94fb061f8786c0b842512106b
Cao Vy <vihcce170371@fpt.edu.vn>
2024-08-16 13:03:06 +0700

    Code split use database.js"

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
-const webRoutes = require('./routes/web')
-const mysql = require('mysql2')
+const webRoutes = require('./routes/web');
+const connection = require('./config/database');
 
 
 const app = express();
@@ -13,22 +13,11 @@ configViewEngine(app, express);
 
 app.use('/', webRoutes)
 
-//test connection
-const connection = mysql.createConnection(
-    {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-    }
-)
-
 connection.query(
     'SELECT * from Users u', (err, result, field) => {
         console.log('error>>>>', err);
         console.log('result>>>>', result);
-        console.log('field>>>>', field);
+        // console.log('field>>>>', field);
     }
 )
 
