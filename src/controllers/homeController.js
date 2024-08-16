51989b3eeaa7b0716654e5e3b726fef64fc2b148
Cao Vy <vihcce170371@fpt.edu.vn>
2024-08-16 14:25:06 +0700

    Code split connection query

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,16 @@
+const connection = require('../config/database')
+
 const getHomepage = (req, res) => {
+    let users = [];
+    connection.query(
+        'SELECT * from Users u', (err, result, field) => {
+            users = result
+            console.log('result>>>>', result);
+        }
+    )
+
+    console.log("Check user>>>", users);
+
     res.send('Hello Express vs ICao Vy!!!<br/> <a href="test">Test Page</a>')
 }
 
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const connection = require('./config/database');
 
 
 const app = express();
@@ -13,13 +12,7 @@ configViewEngine(app, express);
 
 app.use('/', webRoutes)
 
-connection.query(
-    'SELECT * from Users u', (err, result, field) => {
-        console.log('error>>>>', err);
-        console.log('result>>>>', result);
-        // console.log('field>>>>', field);
-    }
-)
+
 
 app.listen(port, hostname, () => {
     console.log(`Sever running at http://${hostname}:${port}/`);
